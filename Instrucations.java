982c18de8f26dd367519dfdabb7a29400d5ba3d5
pjmoradiya <132308798+pjmoradiya@users.noreply.github.com>
2024-12-03 22:29:41 -0600

    Create Instrucations.java

---
+++ Instrucations.java
@@ -0,0 +1,7 @@
+Let's start fresh for the automation tool. 
+
+Okay, so first of all, we have the comparator class, ExcelFileProcessor class. Which does it's operation based on the button used in the UI. Now, I want to automate this. 
+
+I know last time you gave me a code for regressionTester but we need to modify a lot. 
+
+So what happens is that when we are trying to leverage the existing class, it is not storing files at the correct location. 
